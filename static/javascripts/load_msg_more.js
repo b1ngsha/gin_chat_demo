24d936fe325bba88cd570bb6251214f6ee0f6831
b1ngsha <2941125807@qq.com>
2024-12-21 09:39:04 +0800

    feat: render private chat web page data

--- static/javascripts/load_msg_more.js
+++ static/javascripts/load_msg_more.js
@@ -7,7 +7,36 @@ $(document).ready(function() {
         $.ajax({
             url: `/pagination?room_id=${room_id}&offset=${offset}&user_id=${user_id}`,
             success: function(data) {
-                
+                let item = data.data.list;
+                if (item == null) {
+                    layer.msg("no more messsage!");
+                    $("#hidden-chat-list-li-top").attr("data-offset", offset);
+                    $("#chat-list-li-top").hide();
+                    return false;
+                }
+
+                $.each(item, function(index, value) {
+                    if (value.from_user_id == $("#body-room").attr("data-user_id")) {
+                        $("#chat-list-li-top").after(
+                            `<li class="right">
+                                <img src="/static/images/user/${value.avatar_id}.png" alt="">
+                                <b>${value.username}</b>
+                                <i>${value.created_at}</i>
+                                <div class="aaa">${value.content}</div>
+                            </li>`
+                        );
+                    } else {
+                        $("#chat-list-li-top").after(
+                            `<li class="left">
+                                <img src="/static/images/user/${value.avatar_id}.png" alt="">
+                                <b>${value.username}</b>
+                                <i>${value.created_at}</i>
+                                <div class="aaa">${value.content}</div>
+                            </li>`
+                        );
+                    }
+                })
+                $("hidden-chat-list-li-top").attr("data-offset", parseInt(offset) + 100);
             }
         })
     })
